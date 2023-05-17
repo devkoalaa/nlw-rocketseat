eb21ac563714034a6afc3f8a3649fff564fc83cc
Herberticleber Carvalho Valadares <herbert.valadares@ssp.df.gov.br>
2023-05-17 16:57:38 -0300

    aula 2 concluida

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,19 +1,18 @@
 import fastify from 'fastify'
-import { PrismaClient } from '@prisma/client'
+import { memoriesRoutes } from './routes/memories'
+import cors from '@fastify/cors'
 
 const app = fastify()
-const prisma = new PrismaClient()
 
-app.get('/users', async () => {
-  const users = await prisma.user.findMany()
-
-  return users
+app.register(cors, {
+  origin: true,
 })
+app.register(memoriesRoutes)
 
 app
   .listen({
     port: 3333,
   })
   .then(() => {
-    console.log('🚀🐨 HTTP server running on http://localhost:3333')
+    console.log('🐨🚀 HTTP server running on http://localhost:3333')
   })
