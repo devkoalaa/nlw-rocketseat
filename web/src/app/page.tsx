ea2ba3d9045dce5ff8e64ab892faff4db2dedf87
Herberticleber Carvalho Valadares <herbert.valadares@ssp.df.gov.br>
2023-05-17 11:56:59 -0300

    start style web

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,31 +1,81 @@
-'use client'
-import { useState } from 'react'
+import { User } from 'lucide-react'
+import Image from 'next/image'
+import nlwlogo from '../assets/nlw-spacetime-logo.svg'
 
 export default function Home() {
-  const [result, setResult] = useState<any>()
-
-  async function getApi() {
-    fetch('http://localhost:3333/users').then((response) => {
-      response.json().then((data) => {
-        setResult(data)
-      })
-    })
-  }
-
   return (
-    <div className="h-screen bg-zinc-900 p-6 text-zinc-50">
-      <h1 className="text-4xl font-bold">Sua cápsula do tempo</h1>
-      <button className="m-2 rounded bg-gray-700 p-2" onClick={getApi}>
-        Get API
-      </button>
-      {result &&
-        result.map((item: any) => {
-          return (
-            <p className="m-2 bg-zinc-700 text-2xl" key={item.id}>
-              {item.name}
+    <main className="grid min-h-screen grid-cols-2">
+      {/* Left */}
+      <div className="relative flex flex-col items-start justify-between overflow-hidden border-r border-white/10 bg-[url(../assets/bg-stars.svg)] bg-cover p-16 px-28 py-16 ">
+        {/* Blur */}
+        <div className="absolute right-0 top-1/2 h-[288px] w-[526px] -translate-y-1/2 translate-x-1/2 rounded-full bg-purple-700 opacity-50 blur-full" />
+
+        {/* Stripes */}
+        <div className="absolute bottom-0 right-2  top-0 w-2 bg-stripes" />
+
+        {/* Sign In */}
+        <a
+          href=""
+          className="flex items-center gap-3 text-left transition-colors hover:text-gray-50"
+        >
+          <div className="flex h-10 w-10 items-center justify-center rounded-full bg-gray-400">
+            <User className="h-5 w-5 text-gray-500" />
+          </div>
+
+          <p className="max-w-[140px] text-sm leading-snug">
+            <span className="underline">Crie sua conta</span> e salve suas
+            memórias!
+          </p>
+        </a>
+
+        {/* Hero */}
+        <div className="space-y-5">
+          <Image src={nlwlogo} alt="NLW Spacetime" />
+
+          <div className="max-w-[420px] space-y-1">
+            <h1 className="text-5xl font-bold leading-tight text-gray-50">
+              Sua cápsula do tempo
+            </h1>
+            <p className="text-lg leading-relaxed">
+              Colecione momentos marcantes da sua jornada e compartilhe (se
+              quiser) com o mundo!
             </p>
-          )
-        })}
-    </div>
+          </div>
+
+          <a
+            href=""
+            className="inline-block rounded-full bg-green-500 px-5 py-3 font-alt text-sm uppercase leading-none text-black hover:bg-green-600"
+          >
+            CADASTRAR LEMBRANÇA
+          </a>
+        </div>
+
+        {/* Copyright */}
+        <div>
+          Feito por 🐨 no NLW 🚀 da {''}
+          <a
+            target="_blank"
+            rel="noreferrer"
+            className="underline hover:text-gray-100 "
+            href="https://rocketseat.com.br"
+          >
+            Rocketseat
+          </a>
+        </div>
+      </div>
+
+      {/* Right */}
+      <div className="flex flex-col bg-[url(../assets/bg-stars.svg)] bg-cover p-16">
+        <div className="flex flex-1 items-center justify-center">
+          <p className="w-[360px] text-center leading-relaxed">
+            Você ainda não registrou nenhuma lembrança, comece a {''}
+            <a href="" className="underline hover:text-gray-50">
+              criar agora
+            </a>
+            !
+          </p>
+        </div>
+      </div>
+    </main>
   )
 }
