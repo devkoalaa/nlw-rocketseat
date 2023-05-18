84e7afd23827ed3b9c89c4fb3ddbbdceedd883f8
Herberticleber Carvalho Valadares <herbert.valadares@ssp.df.gov.br>
2023-05-18 13:17:52 -0300

    add oauth

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,12 +1,20 @@
+import cors from '@fastify/cors'
+import 'dotenv/config'
 import fastify from 'fastify'
 import { memoriesRoutes } from './routes/memories'
-import cors from '@fastify/cors'
-
+import { authRoutes } from './routes/auth'
+import jwt from '@fastify/jwt'
 const app = fastify()
 
 app.register(cors, {
   origin: true,
 })
+
+app.register(jwt, {
+  secret: 'spacetime',
+})
+
+app.register(authRoutes)
 app.register(memoriesRoutes)
 
 app
---
+++ web/src/components/Copyright.tsx
@@ -0,0 +1,15 @@
+export function Copyright() {
+  return (
+    <div>
+      Feito por 🐨 no NLW 🚀 da {''}
+      <a
+        target="_blank"
+        rel="noreferrer"
+        className="underline hover:text-gray-100 "
+        href="https://rocketseat.com.br"
+      >
+        Rocketseat
+      </a>
+    </div>
+  )
+}
