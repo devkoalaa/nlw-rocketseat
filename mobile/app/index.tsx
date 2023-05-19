f4968726f891e076edc4d90b48622b6a4e0c8b4a
Herberticleber Carvalho Valadares <herbert.valadares@ssp.df.gov.br>
2023-05-19 15:48:50 -0300

    upload

--- mobile/app/index.tsx
+++ mobile/app/index.tsx
@@ -1,22 +1,10 @@
-import { BaiJamjuree_700Bold } from '@expo-google-fonts/bai-jamjuree'
-import {
-  Roboto_400Regular,
-  Roboto_700Bold,
-  useFonts,
-} from '@expo-google-fonts/roboto'
 import { makeRedirectUri, useAuthRequest } from 'expo-auth-session'
-import * as SecureStore from 'expo-secure-store'
-import { StatusBar } from 'expo-status-bar'
-import { styled } from 'nativewind'
-import React, { useEffect } from 'react'
 import { useRouter } from 'expo-router'
-import { ImageBackground, Text, TouchableOpacity, View } from 'react-native'
-import { api } from '../src/lib/api'
-import blurBg from '../src/assets/blur-bg.png'
+import * as SecureStore from 'expo-secure-store'
+import React, { useEffect } from 'react'
+import { Text, TouchableOpacity, View } from 'react-native'
 import NLWLogo from '../src/assets/nlw-spacetime-logo.svg'
-import Stripes from '../src/assets/stripes.svg'
-
-const StyledStripes = styled(Stripes)
+import { api } from '../src/lib/api'
 
 const discovery = {
   authorizationEndpoint: 'https://github.com/login/oauth/authorize',
@@ -27,12 +15,6 @@ const discovery = {
 
 export default function App() {
   const router = useRouter()
-
-  const [hasLoadedFonts] = useFonts({
-    Roboto_400Regular,
-    Roboto_700Bold,
-    BaiJamjuree_700Bold,
-  })
 
   const [, response, signInWithGithub] = useAuthRequest(
     {
@@ -72,16 +54,8 @@ export default function App() {
     }
   }, [response])
 
-  if (!hasLoadedFonts) return null
-
   return (
-    <ImageBackground
-      source={blurBg}
-      className="relative flex-1 items-center bg-gray-900 px-8 py-10"
-      imageStyle={{ position: 'absolute', left: '-100%' }}
-    >
-      <StyledStripes className="absolute left-2" />
-
+    <View className="flex-1 items-center px-8 py-10">
       <View className="flex-1 items-center justify-center gap-6">
         <NLWLogo />
 
@@ -109,8 +83,6 @@ export default function App() {
       <Text className="text-center font-body text-sm leading-relaxed text-gray-200">
         Feito por 🐨 no NLW da Rockeseat 🚀
       </Text>
-
-      <StatusBar style="light" translucent />
-    </ImageBackground>
+    </View>
   )
 }
--- server/src/server.ts
+++ server/src/server.ts
@@ -1,10 +1,22 @@
-import cors from '@fastify/cors'
 import 'dotenv/config'
+
 import fastify from 'fastify'
+import cors from '@fastify/cors'
+import jwt from '@fastify/jwt'
+import multipart from '@fastify/multipart'
 import { memoriesRoutes } from './routes/memories'
 import { authRoutes } from './routes/auth'
-import jwt from '@fastify/jwt'
+import { uploadRoutes } from './routes/upload'
+import { resolve } from 'node:path'
+
 const app = fastify()
+
+app.register(multipart)
+
+app.register(require('@fastify/static'), {
+  root: resolve(__dirname, '../uploads'),
+  prefix: '/uploads',
+})
 
 app.register(cors, {
   origin: true,
@@ -15,6 +27,7 @@ app.register(jwt, {
 })
 
 app.register(authRoutes)
+app.register(uploadRoutes)
 app.register(memoriesRoutes)
 
 app
@@ -23,5 +36,5 @@ app
     host: '0.0.0.0',
   })
   .then(() => {
-    console.log('🐨🚀 HTTP server running on http://localhost:3333')
+    console.log('🚀 HTTP server running on port http://localhost:3333')
   })
